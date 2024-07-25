610575fb894bf7909c5b21a105ab84898e55601f
Moisés <msportela2@hotmail.com>
2024-07-24 22:01:43 -0300

    Criação listar pensamentos

---
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'listar-pensamentos',
+  templateUrl: './listar-pensamentos.component.html',
+  styleUrl: './listar-pensamentos.component.css',
+})
+export class ListarPensamentosComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
